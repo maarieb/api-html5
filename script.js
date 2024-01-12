8bc4c983066dfba213b0d4a4940d5781d7961358
Marie <brierremarie@gmail.com>
2024-01-12 10:34:41 +0100

    plusieurs méthodes ajoutées dans le script.js

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 var drag = document.querySelector('#drag')
 var drop = document.querySelector('#drop')
 
-drop.addEventListener('dragover', (event) => event.preventDefault())
+drop.addEventListener('dragover', (event) => {event.preventDefault()})
+
+drop.addEventListener('dragenter', (event) => {drop.style.backgroundColor = 'white'})
+
+drop.addEventListener('dragleave', (event) => {drop.style.backgroundColor = 'dodgerblue'})
+
+drag.addEventListener('dragstart', (event) => { event.dataTransfer.setData('text/plain', drag.innerHTML)})
+
+drop.addEventListener('drop', (event) => { 
+    const data = event.dataTransfer.getData('text/plain')
+    drop.innerHTML = `${data} ${drop.innerHTML}`
+    drop.style.backgroundColor = 'dodgerblue'
+})
